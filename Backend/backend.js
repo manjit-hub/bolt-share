53e33aaf894446dcc37038fe35be46e97e9b73e4
Manjit Majhi <manjitmajhi156@gmail.com>
2024-08-03 16:00:49 +0530

    updated backend.js

--- Backend/backend.js
+++ Backend/backend.js
@@ -3,15 +3,19 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // IMPORT FILE:
 import { DBConnection } from './database/db.js';
 import routes from './routes/routes.js';
 
-
 // CONFIG
 dotenv.config();
 const app = express();
+
+// Convert import.meta.url to __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Ensure uploads directory exists
 const uploadsDir = path.join(__dirname, 'uploads');
@@ -19,24 +23,24 @@ if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir);
 }
 
-//CORS 
+// CORS 
 app.use(cors({
-    origin : ['https://bolt-share.vercel.app/','http://localhost:5173'],
+    origin: ['https://bolt-share.vercel.app/', 'http://localhost:5173'],
     credentials: true
 }));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // DB Connection
 DBConnection();
 
-app.get('/', (req,res) =>{ // First call to '/' route will be here
+app.get('/', (req, res) => { // First call to '/' route will be here
     res.send("Backend is running!");
-})
+});
 
 app.use('/', routes); // Rest '/' route calls will be forwarded here: Agar '/' badd kuch hoga to routes me redirect hoga
 
 const port = process.env.PORT;
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
